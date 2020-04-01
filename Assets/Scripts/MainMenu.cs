255b9a23a75608f5bdf3e4f2e610d6ae48787292
Андрей Руфов <rufik956@gmail.com>
2020-04-01 16:56:35 +0300

    first page

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnChangePasswordClick()
+    {
+        Debug.Log("Congratulations change password");
+    }
+
+    public void OnEnterClick()
+    {
+        Debug.Log("Congratulations enter");
+    }
+
+    public void OnSignUpClick()
+    {
+        Debug.Log("Congratulations sign up");
+    }
+}
