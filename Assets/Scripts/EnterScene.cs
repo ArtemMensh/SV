bbd54e7dc7882c305dba16d42d48db275b9970dc
Menshakov Artem <artem.menshakov.1999@mail.ru>
2020-04-26 15:31:39 +0300

    D

--- Assets/Scripts/EnterScene.cs
+++ Assets/Scripts/EnterScene.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EnterScene : MonoBehaviour
 {
+    [SerializeField] private Text _textPass;
+    [SerializeField] private Text _textLogin;
+
+    private string _serverPath = "";
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +30,13 @@ public class EnterScene : MonoBehaviour
 
     public void OnEnterClick()
     {
+        var pass = _textPass.text;
+        var login = _textLogin.text;
+
+        //Отправляем запрос для входа
+
+
+
         Debug.Log("Congratulations sign in");
     }
 
--- Assets/Scripts/RegistrationScene.cs
+++ Assets/Scripts/RegistrationScene.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RegistrationScene : MonoBehaviour
 {
+
+    [SerializeField] private InputField _textLogin;
+    [SerializeField] private InputField _textPhone;
+    [SerializeField] private InputField _textPass;
+    [SerializeField] private InputField _textRetryPass;
+    [SerializeField] private InputField _textErrorPass;
+
+    [SerializeField] private GameObject _errorPass;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +36,63 @@ public class RegistrationScene : MonoBehaviour
 
     public void OnClickRegistration()
     {
+        if(!FillField())
+        {
+            ShowErrorPass(true, "Не все поля заполнены");
+            return;
+        }
+        if(_textPass.text.Equals(_textRetryPass.text))
+        {
+            // все верно отправляем запрос на регестрацию
+            StartCoroutine(Send(_textLogin.text, _textPass.text, _textPhone.text));
+        }
+        else
+        {
+            // выводим сообщение об ошибке
+            ShowErrorPass(true, "Пароли не совпадают");
+        }
+    }
 
+    private bool FillField()
+    {
+        if (_textLogin.text.Equals("")) return false;
+        if (_textPass.text.Equals("")) return false;
+        if (_textPhone.text.Equals("")) return false;
+        if (_textRetryPass.text.Equals("")) return false;
+        return true;
+    }
+
+    private void ShowErrorPass(bool IsShow, string massage)
+    {
+        _errorPass.SetActive(IsShow);
+        _textErrorPass.text = massage;
+    }
+
+    public void CloseErrorPass()
+    {
+        ShowErrorPass(false, "");
+    }
+
+    private IEnumerator Send(string login, string pass, string phone)
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("username=" + login));
+        formData.Add(new MultipartFormDataSection("password=" + pass));
+        formData.Add(new MultipartFormDataSection("phone=" + phone));
+        
+        //formData.Add(new MultipartFormFileSection("my file data", "myfile.txt"));
+
+        //UnityWebRequest www = UnityWebRequest.Post("https://sv.egipti.com/api/auth/register", formData);
+        UnityWebRequest www = UnityWebRequest.Post("https://sv.egipti.com/api/auth/register","username=" + login + "&password=" + pass + "&phone="+phone);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
     }
 }
