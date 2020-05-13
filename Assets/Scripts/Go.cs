c22a5252f31f1ccf85dbee7fc4e3d236fc2807b1
Menshakov Artem <artem.menshakov.1999@mail.ru>
2020-05-13 15:31:35 +0300

    Navigate

---
+++ Assets/Scripts/Go.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Go : MonoBehaviour
+{
+    public void GoChildrenScence()
+    {
+        SceneManager.LoadScene("AddChildren");
+    }
+
+    public void GoParentScence()
+    {
+        SceneManager.LoadScene("AddParent");
+    }
+
+    public void GoAddScence()
+    {
+        SceneManager.LoadScene("Add");
+    }
+
+    public void GoMainScence()
+    {
+        SceneManager.LoadScene("MainScence");
+    }
+
+    public void GoProfile()
+    {
+        SceneManager.LoadScene("Profile");
+    }
+}
